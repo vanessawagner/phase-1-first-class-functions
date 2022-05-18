d2b509bf415d2a6d70daab93029bc8418a420bf8
Vanessa Wagner <vmwagner@gmail.com>
2022-05-18 10:18:37 -0500

    commit tree 7b877c084a4967d20ce67100d60cb83d6fd01349

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction (callback) {
+        return callback()
+    }
+
+function returnsANamedFunction () {
+    return receivesAFunction
+}
+
+
+function returnsAnAnonymousFunction () {
+    return function(){
+        
+    }
+}
